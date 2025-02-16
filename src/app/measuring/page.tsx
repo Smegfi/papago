7f92c2dcf59a695c35fda62b54e1c4fa7d0310c0
Filip Šmehyl <fila.smehyl@gmail.com>
2025-02-16 03:32:23 +0100

    main

--- src/app/measuring/page.tsx
+++ src/app/measuring/page.tsx
@@ -1,8 +1,11 @@
 import { getMeasuringAction } from "@/server/actions";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { CreateDeviceDialog } from "../device/create-device";
+import { headers } from "next/headers";
 
 export default async function Page() {
+   headers();
    const measuring = await getMeasuringAction();
    return (
       <>
@@ -16,24 +19,23 @@ export default async function Page() {
                   <TableHead>Data</TableHead>
                </TableRow>
             </TableHeader>
-               <TableBody>
-                  {measuring!.data!.map((item) => (
-                     <TableRow key={item.id}>
+            <TableBody>
+               {measuring!.data!.map((item) => (
+                  <TableRow key={item.id}>
                      <TableCell className="font-bold">{item.deviceId}</TableCell>
                      <TableCell>{item.timestamp?.toLocaleString("cs-CZ", { hour: "2-digit", minute: "2-digit", second: "2-digit" })}</TableCell>
                      <TableCell>
                         <ScrollArea className="h-64 rounded-md border">
                            <pre>
-                              <code className="text-xs">
-                                 {JSON.stringify(item.rawData, null, 2)}
-                              </code>
+                              <code className="text-xs">{JSON.stringify(item.rawData, null, 2)}</code>
                            </pre>
                         </ScrollArea>
                      </TableCell>
                   </TableRow>
-                  ))}
-               </TableBody>
-            </Table>
+               ))}
+            </TableBody>
+         </Table>
+         <CreateDeviceDialog />
       </>
    );
 }
