c838af1d8224b945dca5c13d1cd49d593bccad9b
Smegfi <fila.smehyl@gmail.com>
2025-11-12 09:50:47 +0100

    papago update

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-   /* config options here */
-   cacheMaxMemorySize: 0,
-   output: "standalone",
-   eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
